7c92ddb5720b873b6ec71defa2d7e3f6a011eb60
saikrishna <shanigaramsaikrishnaoo7@gmail.com>
2023-06-28 21:11:14 +0530

    second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,43 @@
-import Speedometer from "./Components/Speedometer";
+import React, { Component } from "react";
+import Login from "./Components/Login";
 
 
-const App = () =>{
-  return(
-    <Speedometer />
-  )
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLogIn: false,
+    };
+  }
+
+  toggleLogin = () => {
+    this.setState((prevState) => ({
+      isLogIn: !prevState.isLogIn,
+    }));
+  }
+
+  render(){
+    const {isLogIn} = this.state;
+    return(
+      <div className="text-center">
+      <center>
+    
+        {isLogIn? 
+        (<div>
+        <Login greeting = "Hello" name="saikrishna" />
+        <p>You are logged in</p>
+        <button className="btn btn-secondary" onClick={this.toggleLogin}>LOGOUT</button>
+        </div>)
+        :
+        (<div>
+        <p>You are logged out</p>
+          <button className="btn btn-primary"  onClick={this.toggleLogin}>LOGIN</button>
+          </div>
+          )}
+        </center>
+      </div>
+    )
+  }
 }
 
 export default App;
